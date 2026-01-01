087851af775b99c5ab5a579aab6a49d23fd0e123
Jefté Medeiros <jefte.medeiros8@gmail.com>
2026-01-01 16:59:51 -0300

    feat: added glass ui components

---
+++ src/components/loading-skeleton.tsx
@@ -0,0 +1,23 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function ExpenseTableSkeleton() {
+  return (
+    <div className="flex flex-col gap-4 mt-4">
+      <div className="flex gap-2">
+        <Skeleton className="flex items-center px-4 justify-between gap-4 rounded-md w-full bg-gray-600 h-12 animate-pulse" />
+        <Skeleton className="flex items-center px-4 justify-between gap-4 rounded-md w-full bg-gray-600 h-12 animate-pulse" />
+        <Skeleton className="flex items-center px-4 justify-between gap-4 rounded-md w-full bg-gray-600 h-12 animate-pulse" />
+      </div>
+      <Skeleton className="flex items-center px-4 justify-between gap-4 rounded-md w-full bg-gray-600 h-20 animate-pulse" />
+      <Skeleton className="flex items-center px-4 justify-between gap-4 rounded-md w-full bg-gray-600 h-20 animate-pulse" />
+      <Skeleton className="flex items-center px-4 justify-between gap-4 rounded-md w-full bg-gray-600 h-20 animate-pulse" />
+      <Skeleton className="flex items-center px-4 justify-between gap-4 rounded-md w-full bg-gray-600 h-20 animate-pulse" />
+    </div>
+  );
+}
+
+export function ProfileCardSkeleton() {
+  return (
+    <Skeleton className="w-32 h-10 bg-gray-700 rounded-md animate-pulse" />
+  );
+}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,33 +2,33 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
 
 export function moneyFormatter(value: number) {
-	return new Intl.NumberFormat("pt-BR", {
-		style: "currency",
-		currency: "BRL",
-	}).format(value);
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
 }
 
 const expenseExamples = [
-	"Ex: Escola do meu filho",
-	"Ex: Compras do mês",
-	"Ex: Conta de luz",
-	"Ex: Conta de água",
-	"Ex: Conta de internet",
-	"Ex: Conta de telefone",
-	"Ex: Conta de celular",
-	"Ex: Conta de cartão de crédito",
-	"Ex: Conta de cartão de débito",
-	"Ex: Conta de supermercado",
-	"Ex: Conta de farmácia",
-	"Ex: Conta de combustível",
-	"Ex: Conta de gás",
+  "Ex: Escola do meu filho",
+  "Ex: Compras do mês",
+  "Ex: Conta de luz",
+  "Ex: Conta de água",
+  "Ex: Conta de internet",
+  "Ex: Conta de telefone",
+  "Ex: Conta de celular",
+  "Ex: Conta de cartão de crédito",
+  "Ex: Conta de cartão de débito",
+  "Ex: Conta de supermercado",
+  "Ex: Conta de farmácia",
+  "Ex: Conta de combustível",
+  "Ex: Conta de gás",
 ];
 
 export const generateExpenseExampleMessage = () => {
-	const randomMessage = Math.random() * expenseExamples.length;
-	return expenseExamples[Math.floor(randomMessage)];
+  const randomMessage = Math.random() * expenseExamples.length;
+  return expenseExamples[Math.floor(randomMessage)];
 };
